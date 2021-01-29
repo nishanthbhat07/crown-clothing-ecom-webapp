dc391aa2108fce605baf5b236e0edbb8d627d783
Nishanth Bhat <nishanthbhat07@gmail.com>
2021-01-29 20:04:32 +0530

    Solving deployment problems-I

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import CartIcon from "../cart-icon/cart-icon.component";
-import { auth } from "../../firebase/firebase.utils";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { createStructuredSelector } from "reselect";
